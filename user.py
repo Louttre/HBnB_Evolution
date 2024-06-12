7b8d4cff0a0f7f8e98dcd51e5d6cab34e7ac8840
R. Johan <105952153+Louttre@users.noreply.github.com>
2024-06-12 14:12:54 +0200

    Update user.py

--- user.py
+++ user.py
@@ -1,13 +1,13 @@
-from .crud import CRUD
+import json
 import uuid
 from datetime import datetime
-
+from .crud import CRUD
 
 class User(CRUD):
 
-    storage = {}
-    def __init__(self, first_name, last_name, email, password):
+    storage_file = 'user_storage.json'
 
+    def __init__(self, first_name, last_name, email, password):
         self.id = str(uuid.uuid4())
         self.first_name = first_name
         self.last_name = last_name
@@ -23,43 +23,75 @@ class User(CRUD):
 
     def to_dict(self):
         return {
-                'id': self.id,
-                'first_name': self.first_name,
-                'last_name': self.last_name,
-                'email': self.email,
-                'password': self.password,
-                'hosted_places': self.hosted_places,
-                'reviews': self.reviews,
-                'created_at': self.created_at.isoformat(),
-                'updated_at': self.updated_at.isoformat()
-                }
+            'id': self.id,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'password': self.password,
+            'hosted_places': self.hosted_places,
+            'reviews': self.reviews,
+            'created_at': self.created_at.isoformat(),
+            'updated_at': self.updated_at.isoformat()
+        }
 
     @classmethod
-    def create(self, data):
+    def read_storage(cls):
+        try:
+            with open(cls.storage_file, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {}
+        except json.JSONDecodeError:
+            return {}
+
+    @classmethod
+    def write_storage(cls, storage):
+        with open(cls.storage_file, 'w') as file:
+            json.dump(storage, file, indent=4)
+
+    @classmethod
+    def create(cls, data):
+        storage = cls.read_storage()
         user = User(**data)
-        User.storage[user.id] = user
+        storage[user.id] = user.to_dict()
+        cls.write_storage(storage)
         return user
 
     @classmethod
     def read(cls, id):
-        return cls.storage.get(id)
+        storage = cls.read_storage()
+        user_data = storage.get(id)
+        if user_data:
+            return User(**user_data)
+        return None
 
     @classmethod
     def update(cls, id, data):
-        user = cls.storage.get(id)
-        if user:
+        storage = cls.read_storage()
+        user_data = storage.get(id)
+        if user_data:
+            user = User(**user_data)
             for key, value in data.items():
-                if hasattr(User, key):
-                    setattr(User, key, value)
+                if hasattr(user, key):
+                    setattr(user, key, value)
             user.updated_at = datetime.utcnow()
+            storage[id] = user.to_dict()
+            cls.write_storage(storage)
             return user
-        return None                                                                                                                                                                                         
-    @classmethod                                                                                            
+        return None
+
+    @classmethod
     def get_user(cls, id):
         user = cls.read(id)
         if user is None:
             raise ValueError("User not found")
+        return user
 
     @classmethod
     def delete(cls, id):
-        return cls.storage.pop(id, None)
+        storage = cls.read_storage()
+        if id in storage:
+            user = storage.pop(id)
+            cls.write_storage(storage)
+            return User(**user)
+        return None
